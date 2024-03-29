537264e697f2d6749d9a8edda8decf689eb06f61
Janett Gerrard <mikanchik@gmail.com>
2024-03-29 18:42:13 +0000

    create data object for existing reviews

---
+++ reviewData.js
@@ -0,0 +1,20 @@
+const reviews = [
+  {
+    name: "Genuinely horrendous.",
+    comment: `Where to start; I could lead with being beheaded by the Bork-a-naughts one week into the cruise, or should I
+    begin with the water-boarding on planet Xing-Gong-32T that I received instead?<br> The marmite was useless and
+    I didn't even get to bite into my second egg. Would not recommend, daylight robbery and they have the cheek to
+    make you pay for parking.`,
+    rating: 1,
+    img: ""
+  },
+  {
+    name: "DO NOT CHOOSE ROGER MOORE!!",
+    comment: `Absolute lunatic. The guy has no idea what's going on, he rambles on about someone named Hugo Drax and shoots
+    anything that moves; worst tour guide EVER.`,
+    rating: 1,
+    img: ""
+  }
+]
+
+export default reviews;
