d2ef8cdef9e5029211ad74f405d468d5b3b60915
Janett Gerrard <mikanchik@gmail.com>
2024-04-02 15:43:50 +0100

    rework script.js to fix carousels

--- script.js
+++ script.js
@@ -1,18 +1,26 @@
 // JavaScript for carousel functionality
-const prevButton = document.querySelector('.prev');
-const nextButton = document.querySelector('.next');
-const slides = document.querySelector('.slides');
+const carousels = document.querySelectorAll('.carousel');
 
-prevButton.addEventListener('click', () => {
-    slides.scrollBy({
-        left: -slides.offsetWidth,
-        behavior: 'smooth'
-    });
-});
+const moveSlidesBack = (slides) => {
+  console.log("the function was called")
+  slides.scrollBy({
+      left: -slides.offsetWidth,
+      behavior: 'smooth'
+  });
+}
 
-nextButton.addEventListener('click', () => {
-    slides.scrollBy({
-        left: slides.offsetWidth,
-        behavior: 'smooth'
-    });
-});
+const moveSlidesForth = (slides) => {
+  slides.scrollBy({
+      left: slides.offsetWidth,
+      behavior: 'smooth'
+  });
+}
+
+carousels.forEach((carousel) => {
+  let prevButton = carousel.querySelector('.prev');
+  let nextButton = carousel.querySelector('.next');
+  let slides = carousel.querySelector('.slides');
+
+  prevButton.addEventListener("click", () => moveSlidesBack(slides))
+  nextButton.addEventListener("click", () => moveSlidesForth(slides))
+})
