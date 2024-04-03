c4b180acd800431b5c6077a87bbcb769d15ef4ba
Janett Gerrard <mikanchik@gmail.com>
2024-04-03 16:40:59 +0100

    disable previous button when at the start of the slides

--- carousel.js
+++ carousel.js
@@ -1,14 +1,19 @@
 // JavaScript for carousel functionality
 const carousels = document.querySelectorAll('.carousel');
 
-const moveSlidesBack = (slides) => {
+const moveSlidesBack = (slides, prev, next) => {
   slides.scrollBy({
       left: -slides.offsetWidth,
       behavior: 'smooth'
-  });
+  })
+  if(slides.scrollLeft === slides.offsetWidth) {
+    prev.disabled = true
+  } else {
+    next.disabled = false
+  }
 }
 
-const moveSlidesForth = (slides) => {
+const moveSlidesForth = (slides, prev, next) => {
   slides.scrollBy({
       left: slides.offsetWidth,
       behavior: 'smooth'
@@ -20,6 +25,7 @@ carousels.forEach((carousel) => {
   let nextButton = carousel.querySelector('.next');
   let slides = carousel.querySelector('.slides');
 
-  prevButton.addEventListener("click", () => moveSlidesBack(slides))
-  nextButton.addEventListener("click", () => moveSlidesForth(slides))
+  prevButton.addEventListener("click", () => moveSlidesBack(slides, prevButton, nextButton))
+  nextButton.addEventListener("click", () => moveSlidesForth(slides, prevButton, nextButton))
+  prevButton.disabled = true;
 })
